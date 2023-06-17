d03e965cb07969db44421e3453ebed7f3214f1f7
tide525 <tide525@outlook.jp>
2023-06-17 17:30:46 +0900

    update game

--- src/game.ts
+++ src/game.ts
@@ -2,23 +2,24 @@ import * as Phaser from 'phaser';
 
 const TOKENS = ['の', '、', '。', 'に', 'は', 'を', 'が', 'と', 'で', '年', '・', '（', '）', 'さ', 'して', 'した', ' いる', 'する', 'も', '「', '」', '月', 'から', 'れた', '日', 'こと', 'し', 'である', 'れて', 'や', '２', '１', 'いた', 'ある', '『', '』', 'れる', 'など', '３', '−', '', 'この', 'ない', 'ため', '日本', '人', '”', 'より', '４', 'れ', '第', 'いう', '者', 'その', 'なった', 'もの', 'へ', '後', 'まで', ' また', '市', 'なる', '５', '中', '６', '一', '同', '県', 'これ', '１０', '７', '内', '８', 'なって', 'おり', 'よる', '９', '大学', 'つ', '大', '国', 'よって', '時', '１２', 'であった', 'か', '家', '駅', 'ように', 'ら', '現在', '的な', '本', '１１', '軍', '上', '：', '化', 'であり', '的に', 'なり', '放送', '名', '性', 'ず', '部', '回', '目', '町', '時代', 'それ', 'なかった', 'おいて', '世界', '代', '線', '間', '戦', 'でも', 'られる', 'あり', '会', '場合', '行わ', '二', 'ついて', '所', 'その後', '東京', '前', '多く', '州', 'だった', '地', 'あった', 'なく', 'しかし', 'い', 'られた', '号', '数', 'できる', '的', '作品', '彼', '選手', '他', '｜', '使用', '機', '昭和', '語', 'られて', '郡', '位', '研究', '当時', '存在', '新', '元', 'アメリカ', '長', '側', '三', '活動', '映画', '初', '学校', ' 社', '等', '１５', '，', '全', '下', '番組', '呼ば', '東', '区', '２０', '会社', '出身', 'および', '／', '車', '約', 'のみ', '代表', '形', '権', 'なお', 'テレビ', '西', '系', '発売', '型', '以下', '地域', '法', '開発', '１４', '歳', '作', '１３', '１６', '中心', 'チーム', 'たち', '北', '分', 'られ', '館', '鉄道', 'おける', '時間', '以降', '３０', 'ドイツ', '小', '出場', '一部', '南', '用', 'さらに', '！', '発表', '度', '試合', '平成', '＝', 'だ', '高', '学', '賞', '局', '登場', '大会', '版', ' 開始', '（）', '次', 'フランス', '川', '際', '点', '式', '関係', '曲', '参加', '記録', '体', 'よう な', '所属', '％', '多い', '利用', 'ｍ', 'でき', 'だけ', '世', '．', '１８', '１７', 'シリーズ', ' 明治', '以上', '事', 'ゲーム', '見', 'お', '力', '##な', 'な', '音楽', 'せ', 'シーズン', '開催', '##子', 'リーグ', '島', 'ともに', 'にて', '各', '級', '国際', 'いった', '監督', '氏', 'イギリス', ' 山', '両', '世紀', '問題', '村', '２０１０', '旧', '対して', '優勝', '知ら', '都市', '１９', '行う', '金', '場', '道', '一般', '中国', '物', '出演', '設置', 'ので', 'せる', '持つ', '地方', '事業', '社会', '卒業', '戦争', '共に', '官', '委員']
 
+const NUM_EPOCHS = 3;
+const EPOCH_SECONDS = 5;
+
+const MAX_LOSS = 10;
+
+const TOKEN_ALIVE_SECONDS = 1;
+const TOKEN_BUFFER_SECONDS = 0.1;
+const TOKEN_DEAD_SECONDS = 0.5;
+
+const NUM_TOKENS_PER_EPOCH = 10;
+
 export default class Game extends Phaser.Scene {
-    numEpochs = 3;
-    epochSeconds = 5;
-    totalEpochSecond = this.numEpochs * this.epochSeconds;
-
-    maxLoss = 10.0;
-
     epoch: number;
 
     hiddenScore: number;
     loss: number;
 
-    tokenSeconds: number;
-    tokenBufferSeconds: number;
-
-    barWidth: number;
-    barHeight: number;
+    maxBarWidth: number;
 
     bar: Phaser.GameObjects.Rectangle;
 
@@ -49,11 +50,8 @@ export default class Game extends Phaser.Scene {
         this.epoch = 0;
 
         this.hiddenScore = 0;
-        this.loss = this.maxLoss;
-    
-        this.tokenSeconds = 1;
-        this.tokenBufferSeconds = 0.1;
-        
+        this.loss = MAX_LOSS;
+
         this.cameras.main.setBackgroundColor('#efefef');
 
         this.anims.create({
@@ -66,22 +64,7 @@ export default class Game extends Phaser.Scene {
 
         this.sound.add('click');
 
-        this.barWidth = 240;
-        this.barHeight = 20;
-
-        const barX = 320 - this.barWidth / 2;
-        const barY = 40;
-
-        this.bar = this.add.rectangle(barX, barY, 0, this.barHeight, 0).setOrigin(0, 0.5);
-
-        const barTextLeftX = 40;
-        const barTextRightX = 160;
-        const barTextConfig = { color: 'black' };
-
-        this.add.text(barTextLeftX, barY, 'エポック', barTextConfig).setOrigin(0, 0.5);
-        this.percentText = this.add.text(barTextRightX, barY, '', barTextConfig).setOrigin(1, 0.5);
-        this.epochText = this.add.text(640 - barTextRightX, barY, '', barTextConfig).setOrigin(0, 0.5);
-        this.secondText = this.add.text(640 - barTextLeftX, barY, '', barTextConfig).setOrigin(1, 0.5);
+        this.createBar();
 
         // ロス
         this.lossText = this.add.text(40, 360 - 40, '', {
@@ -96,28 +79,57 @@ export default class Game extends Phaser.Scene {
     update() {
         this.lossText.setText('バリデーションロス: ' + this.loss.toFixed(2));
 
+        this.bar.setSize(this.maxBarWidth * this.epoch / NUM_EPOCHS, this.bar.height);
+
+        this.percentText.setText(Math.floor(this.epoch / NUM_EPOCHS * 100) + '%');
+        this.epochText.setText(this.epoch + '/' + NUM_EPOCHS);
+
         const second = Math.floor(this.epochEvent.getElapsed() / 1000);
-        this.bar.setSize(this.barWidth * this.epoch / this.numEpochs, this.barHeight);
 
-        this.percentText.setText(Math.floor(this.epoch / this.numEpochs * 100) + '%');
-        this.epochText.setText(this.epoch + '/' + this.numEpochs);
+        const elapsedSecond = this.epoch * EPOCH_SECONDS + second;
+        const leftSecond = NUM_EPOCHS * EPOCH_SECONDS - elapsedSecond;
 
-        const totalSecond = second + this.epoch * this.epochSeconds;
-        const leftSecond = this.totalEpochSecond - totalSecond;
-        this.secondText.setText(totalSecond.toString().padStart(2, '0') + '<' + leftSecond.toString().padStart(2, '0'));
+        this.secondText.setText(
+            elapsedSecond.toString().padStart(2, '0')
+            + '<' + leftSecond.toString().padStart(2, '0')
+        );
     }
 
+    /**
+     * バーの表示
+     */
+    createBar() {
+        this.maxBarWidth = 240;
+
+        const barX = 320 - this.maxBarWidth / 2;
+        const barY = 40;
+
+        this.bar = this.add.rectangle(barX, barY, 0, 20, 0).setOrigin(0, 0.5);
+
+        const barTextLeftX = 40;
+        const barTextRightX = 160;
+        const barTextConfig = { color: 'black' };
+
+        this.add.text(barTextLeftX, barY, 'エポック', barTextConfig).setOrigin(0, 0.5);
+        this.percentText = this.add.text(barTextRightX, barY, '', barTextConfig).setOrigin(1, 0.5);
+        this.epochText = this.add.text(640 - barTextRightX, barY, '', barTextConfig).setOrigin(0, 0.5);
+        this.secondText = this.add.text(640 - barTextLeftX, barY, '', barTextConfig).setOrigin(1, 0.5);
+    }
+
+    /**
+     * エポックの開始
+     */
     epochHandler() {
         this.epochEvent = this.time.addEvent({
-            delay: this.epochSeconds * 1000,
+            delay: EPOCH_SECONDS * 1000,
             callback: this.epochCallback,
             callbackScope: this,
         });
 
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < NUM_TOKENS_PER_EPOCH; i++) {
             const appearSeconds = Phaser.Math.FloatBetween(
-                0 + this.tokenBufferSeconds,
-                this.epochSeconds - (this.tokenSeconds + this.tokenBufferSeconds)
+                0 + TOKEN_BUFFER_SECONDS,
+                EPOCH_SECONDS - (TOKEN_ALIVE_SECONDS + TOKEN_BUFFER_SECONDS)
             );
 
             this.time.addEvent({
@@ -128,7 +140,11 @@ export default class Game extends Phaser.Scene {
         }
     }
 
+    /**
+     * エポックが終わったとき
+     */
     epochCallback() {
+        console.log(this.hiddenScore);
         if (this.epoch < 2) {
             this.epoch += 1;
             this.events.emit('startEpoch');
@@ -139,40 +155,54 @@ export default class Game extends Phaser.Scene {
         }
     }
 
+    /**
+     * トークンの処理
+     */
     tokenHandler() {
         const x = Phaser.Math.Between(80, 560);
         const y = Phaser.Math.Between(80, 200);
 
-        const text = this.add.text(x, y, '[MASK]', {
+        const tokenText = this.add.text(x, y, '[MASK]', {
             fontSize: '2em',
             color: '#2b5283'
         }).setOrigin(0.5);
-        text.setInteractive();
 
+        tokenText.setInteractive();
+
+        // 時間切れ
         const disappearEvent = this.time.addEvent({
-            delay: this.tokenSeconds * 1000,
+            delay: TOKEN_ALIVE_SECONDS * 1000,
             callback: function () {
-                text.setVisible(false);
+                tokenText.setVisible(false);
             },
             callbackScope: this
         });
 
-        text.on('pointerdown', function () {
+        // クリック
+        tokenText.on('pointerdown', function () {
             this.sound.play('click');
 
-            this.hiddenScore += Math.floor(this.tokenSeconds * 1000 - disappearEvent.getElapsed());  // 平均333
-            this.loss = this.maxLoss * Math.exp(-2 * this.hiddenScore / 10000);  // 平均の合計で正規化、適当にスケール
-
-            text.removeInteractive();
+            // 残り時間をスコアとして加算
+            this.hiddenScore += Math.floor(
+                TOKEN_ALIVE_SECONDS * 1000 - disappearEvent.getElapsed()
+            );
+            
+            tokenText.removeInteractive();
             disappearEvent.remove(false);
 
-            const token = TOKENS[Phaser.Math.Between(0, TOKENS.length - 1)];
-            text.setText(token)
+            // クリックの確率に、線形で増加する密度関数を仮定
+            // このとき、スコアの平均は最大値の1/3
+            const avgHiddenScore = TOKEN_ALIVE_SECONDS * 1000 / 3;
+            const sumAvgHiddenScore = avgHiddenScore * NUM_TOKENS_PER_EPOCH * NUM_EPOCHS;
+            this.loss = MAX_LOSS * Math.exp(-2 * this.hiddenScore / sumAvgHiddenScore);  // 適当にスケール
+
+            const token = TOKENS[Math.floor(Math.random() * TOKENS.length)];
+            tokenText.setText(token)
 
             this.time.addEvent({
-                delay: 0.5 * 1000,
+                delay: TOKEN_DEAD_SECONDS * 1000,
                 callback: function () {
-                    text.setVisible(false);
+                    tokenText.setVisible(false);
                 },
                 callbackScope: this
             });
